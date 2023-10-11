74099f0874e735cd2b597f533c34a694dbeda235
Uday <udaikiran0070@gmail.com>
2023-10-11 12:33:47 +0530

    client

--- src/components/create/Update.jsx
+++ src/components/create/Update.jsx
@@ -81,7 +81,7 @@ const Update = () => {
             formData.append('file', file);
     
             try {
-              const response = await fetch('https://blog-h1ujk4qbz-uday-kiran-rs-projects.vercel.app/file/upload', {
+              const response = await fetch('https://blog-api-ashen-pi.vercel.app/file/upload', {
                 method: 'POST',
                 body: formData,
               });
